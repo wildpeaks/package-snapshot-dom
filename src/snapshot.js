cd7266f8e55c77413b2ae466da1f057d15957c8a
Cecile Muller <cecilemuller@users.noreply.github.com>
2019-12-23 03:48:03 +0100

    chore: prettify sources

--- src/snapshot.js
+++ src/snapshot.js
@@ -1,5 +1,4 @@
-'use strict';
-
+"use strict";
 
 /**
  * Convert a DOM element to a simpler JSON tree.
@@ -7,30 +6,30 @@
  * @param  {Boolean} skipEmpty Skips node values that evaluate to false (undefined and empty strings)
  * @return {Object}
  */
-function toJSON(node, skipEmpty){
+function toJSON(node, skipEmpty) {
 	const serialized = {};
-	const isValid = (typeof node === 'object') && (node !== null);
-	if (isValid){
+	const isValid = typeof node === "object" && node !== null;
+	if (isValid) {
 		// https://developer.mozilla.org/en-US/docs/Web/API/Node/nodeType#Node_type_constants
 		// serialized.nodeType = node.nodeType;
-		if (node.tagName){
+		if (node.tagName) {
 			serialized.tagName = node.tagName.toLowerCase();
-		} else if (node.nodeName){
+		} else if (node.nodeName) {
 			serialized.nodeName = node.nodeName;
 		}
-		if (node.nodeValue){
+		if (node.nodeValue) {
 			serialized.nodeValue = node.nodeValue;
 		}
 
 		const attrs = node.attributes;
-		if (attrs){
+		if (attrs) {
 			const l = attrs.length;
-			if (l > 0){
+			if (l > 0) {
 				const aggregated = {};
-				for (let i = 0; i < l; i++){
+				for (let i = 0; i < l; i++) {
 					const attr = attrs[i];
 					const skip = skipEmpty && !attr.nodeValue;
-					if (!skip){
+					if (!skip) {
 						aggregated[attr.nodeName] = attr.nodeValue;
 					}
 				}
@@ -39,11 +38,11 @@ function toJSON(node, skipEmpty){
 		}
 
 		const {childNodes} = node;
-		if (childNodes){
+		if (childNodes) {
 			const l = childNodes.length;
-			if (l > 0){
+			if (l > 0) {
 				const aggregated = new Array(l);
-				for (let i = 0; i < l; i++){
+				for (let i = 0; i < l; i++) {
 					aggregated[i] = toJSON(childNodes[i], skipEmpty);
 				}
 				serialized.childNodes = aggregated;
@@ -53,6 +52,4 @@ function toJSON(node, skipEmpty){
 	return serialized;
 }
 
-
 module.exports.toJSON = toJSON;
-
--- test/snapshot.spec.js
+++ test/snapshot.spec.js
@@ -1,252 +1,220 @@
 /* eslint-env node, mocha */
 /* global document */
-'use strict';
-const {deepStrictEqual} = require('assert');
-const {JSDOM} = require('jsdom');
-const snapshot = require('..');
+"use strict";
+const {deepStrictEqual} = require("assert");
+const {JSDOM} = require("jsdom");
+const snapshot = require("..");
 
-
-describe('toJSON', () => {
-
+describe("toJSON", () => {
 	// Reset DOM
 	before(() => {
-		const dom = new JSDOM('<!DOCTYPE html><html><head></head><body></body></html>');
+		const dom = new JSDOM("<!DOCTYPE html><html><head></head><body></body></html>");
 		global.document = dom.window.document;
 	});
 
-	it('Missing element', () => {
+	it("Missing element", () => {
 		deepStrictEqual(snapshot.toJSON(), {});
 	});
-	it('Invalid element (0)', () => {
+	it("Invalid element (0)", () => {
 		deepStrictEqual(snapshot.toJSON(0), {});
 	});
-	it('Invalid element (1)', () => {
+	it("Invalid element (1)", () => {
 		deepStrictEqual(snapshot.toJSON(1), {});
 	});
-	it('Invalid element (false)', () => {
+	it("Invalid element (false)", () => {
 		deepStrictEqual(snapshot.toJSON(false), {});
 	});
-	it('Invalid element (true)', () => {
+	it("Invalid element (true)", () => {
 		deepStrictEqual(snapshot.toJSON(true), {});
 	});
-	it('Invalid element (null)', () => {
+	it("Invalid element (null)", () => {
 		deepStrictEqual(snapshot.toJSON(null), {});
 	});
-	it('Invalid element (undefined)', () => {
-		deepStrictEqual(snapshot.toJSON(undefined), {}); // eslint-disable-line no-undefined
+	it("Invalid element (undefined)", () => {
+		deepStrictEqual(snapshot.toJSON(undefined), {});
 	});
 
-	it('Empty body', () => {
-		document.body.innerHTML = '';
-		deepStrictEqual(
-			snapshot.toJSON(document.body),
-			{
-				tagName: 'body'
-			}
-		);
+	it("Empty body", () => {
+		document.body.innerHTML = "";
+		deepStrictEqual(snapshot.toJSON(document.body), {
+			tagName: "body"
+		});
 	});
 
-	it('Single paragraph in body', () => {
+	it("Single paragraph in body", () => {
 		document.body.innerHTML = '<p class="test1"></p>';
-		deepStrictEqual(
-			snapshot.toJSON(document.body),
-			{
-				tagName: 'body',
-				childNodes: [
-					{
-						tagName: 'p',
-						attributes: {
-							class: 'test1'
-						}
+		deepStrictEqual(snapshot.toJSON(document.body), {
+			tagName: "body",
+			childNodes: [
+				{
+					tagName: "p",
+					attributes: {
+						class: "test1"
 					}
-				]
-			}
-		);
-	});
-
-	it('Single paragraph in detached element', () => {
-		const element = document.createElement('p');
-		element.className = 'test2';
-		deepStrictEqual(
-			snapshot.toJSON(element),
-			{
-				tagName: 'p',
-				attributes: {
-					class: 'test2'
 				}
-			}
-		);
+			]
+		});
 	});
 
-	it('Nested elements in body', () => {
+	it("Single paragraph in detached element", () => {
+		const element = document.createElement("p");
+		element.className = "test2";
+		deepStrictEqual(snapshot.toJSON(element), {
+			tagName: "p",
+			attributes: {
+				class: "test2"
+			}
+		});
+	});
+
+	it("Nested elements in body", () => {
 		document.body.innerHTML = '<div class="outer"><div class="inner"></div><p></p></div>';
-		deepStrictEqual(
-			snapshot.toJSON(document.body),
-			{
-				tagName: 'body',
-				childNodes: [
-					{
-						tagName: 'div',
-						attributes: {
-							class: 'outer'
+		deepStrictEqual(snapshot.toJSON(document.body), {
+			tagName: "body",
+			childNodes: [
+				{
+					tagName: "div",
+					attributes: {
+						class: "outer"
+					},
+					childNodes: [
+						{
+							tagName: "div",
+							attributes: {
+								class: "inner"
+							}
 						},
-						childNodes: [
-							{
-								tagName: 'div',
-								attributes: {
-									class: 'inner'
-								}
-							},
-							{
-								tagName: 'p'
-							}
-						]
-					}
-				]
-			}
-		);
+						{
+							tagName: "p"
+						}
+					]
+				}
+			]
+		});
 	});
 
-	it('Text fragment', () => {
-		document.body.innerHTML = '<p>Hello World</p>';
-		deepStrictEqual(
-			snapshot.toJSON(document.body),
-			{
-				tagName: 'body',
-				childNodes: [
-					{
-						tagName: 'p',
-						childNodes: [
-							{
-								nodeName: '#text',
-								nodeValue: 'Hello World'
-							}
-						]
-					}
-				]
-			}
-		);
+	it("Text fragment", () => {
+		document.body.innerHTML = "<p>Hello World</p>";
+		deepStrictEqual(snapshot.toJSON(document.body), {
+			tagName: "body",
+			childNodes: [
+				{
+					tagName: "p",
+					childNodes: [
+						{
+							nodeName: "#text",
+							nodeValue: "Hello World"
+						}
+					]
+				}
+			]
+		});
 	});
 
-	it('Attributes', () => {
+	it("Attributes", () => {
 		document.body.innerHTML = '<button role="heading" style="color: green">Search</button>';
-		deepStrictEqual(
-			snapshot.toJSON(document.body),
-			{
-				tagName: 'body',
-				childNodes: [
-					{
-						tagName: 'button',
-						attributes: {
-							role: 'heading',
-							style: 'color: green'
-						},
-						childNodes: [
-							{
-								nodeName: '#text',
-								nodeValue: 'Search'
-							}
-						]
-					}
-				]
-			}
-		);
+		deepStrictEqual(snapshot.toJSON(document.body), {
+			tagName: "body",
+			childNodes: [
+				{
+					tagName: "button",
+					attributes: {
+						role: "heading",
+						style: "color: green"
+					},
+					childNodes: [
+						{
+							nodeName: "#text",
+							nodeValue: "Search"
+						}
+					]
+				}
+			]
+		});
 	});
 
-	it('Duplicated attribute (alphabetic order)', () => {
+	it("Duplicated attribute (alphabetic order)", () => {
 		document.body.innerHTML = '<div class="AAA" class="ZZZ"></div>';
-		deepStrictEqual(
-			snapshot.toJSON(document.body),
-			{
-				tagName: 'body',
-				childNodes: [
-					{
-						tagName: 'div',
-						attributes: {
-							class: 'AAA'
-						}
+		deepStrictEqual(snapshot.toJSON(document.body), {
+			tagName: "body",
+			childNodes: [
+				{
+					tagName: "div",
+					attributes: {
+						class: "AAA"
 					}
-				]
-			}
-		);
+				}
+			]
+		});
 	});
-	it('Duplicated attribute (reverse order)', () => {
+	it("Duplicated attribute (reverse order)", () => {
 		document.body.innerHTML = '<div class="ZZZ" class="AAA"></div>';
-		deepStrictEqual(
-			snapshot.toJSON(document.body),
-			{
-				tagName: 'body',
-				childNodes: [
-					{
-						tagName: 'div',
-						attributes: {
-							class: 'ZZZ'
-						}
+		deepStrictEqual(snapshot.toJSON(document.body), {
+			tagName: "body",
+			childNodes: [
+				{
+					tagName: "div",
+					attributes: {
+						class: "ZZZ"
 					}
-				]
-			}
-		);
+				}
+			]
+		});
 	});
 
-	it('Empty attributes (default behavior)', () => {
-		document.body.innerHTML = '<img data-param1 data-param2="" data-param3="  " data-param4="false" data-param5=false />';
-		deepStrictEqual(
-			snapshot.toJSON(document.body),
-			{
-				tagName: 'body',
-				childNodes: [
-					{
-						tagName: 'img',
-						attributes: {
-							'data-param1': '',
-							'data-param2': '',
-							'data-param3': '  ',
-							'data-param4': 'false',
-							'data-param5': 'false'
-						}
+	it("Empty attributes (default behavior)", () => {
+		document.body.innerHTML =
+			'<img data-param1 data-param2="" data-param3="  " data-param4="false" data-param5=false />';
+		deepStrictEqual(snapshot.toJSON(document.body), {
+			tagName: "body",
+			childNodes: [
+				{
+					tagName: "img",
+					attributes: {
+						"data-param1": "",
+						"data-param2": "",
+						"data-param3": "  ",
+						"data-param4": "false",
+						"data-param5": "false"
 					}
-				]
-			}
-		);
+				}
+			]
+		});
 	});
-	it('Empty attributes (skip: false)', () => {
-		document.body.innerHTML = '<img data-param1 data-param2="" data-param3="  " data-param4="false" data-param5=false />';
-		deepStrictEqual(
-			snapshot.toJSON(document.body, false),
-			{
-				tagName: 'body',
-				childNodes: [
-					{
-						tagName: 'img',
-						attributes: {
-							'data-param1': '',
-							'data-param2': '',
-							'data-param3': '  ',
-							'data-param4': 'false',
-							'data-param5': 'false'
-						}
+	it("Empty attributes (skip: false)", () => {
+		document.body.innerHTML =
+			'<img data-param1 data-param2="" data-param3="  " data-param4="false" data-param5=false />';
+		deepStrictEqual(snapshot.toJSON(document.body, false), {
+			tagName: "body",
+			childNodes: [
+				{
+					tagName: "img",
+					attributes: {
+						"data-param1": "",
+						"data-param2": "",
+						"data-param3": "  ",
+						"data-param4": "false",
+						"data-param5": "false"
 					}
-				]
-			}
-		);
+				}
+			]
+		});
 	});
-	it('Empty attributes (skip: true)', () => {
-		document.body.innerHTML = '<img data-param1 data-param2="" data-param3="  " data-param4="false" data-param5=false />';
-		deepStrictEqual(
-			snapshot.toJSON(document.body, true),
-			{
-				tagName: 'body',
-				childNodes: [
-					{
-						tagName: 'img',
-						attributes: {
-							'data-param3': '  ',
-							'data-param4': 'false',
-							'data-param5': 'false'
-						}
+	it("Empty attributes (skip: true)", () => {
+		document.body.innerHTML =
+			'<img data-param1 data-param2="" data-param3="  " data-param4="false" data-param5=false />';
+		deepStrictEqual(snapshot.toJSON(document.body, true), {
+			tagName: "body",
+			childNodes: [
+				{
+					tagName: "img",
+					attributes: {
+						"data-param3": "  ",
+						"data-param4": "false",
+						"data-param5": "false"
 					}
-				]
-			}
-		);
+				}
+			]
+		});
 	});
 });
