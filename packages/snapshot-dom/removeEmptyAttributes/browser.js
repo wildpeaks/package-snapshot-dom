e5cfb241374cfd9828b413fd94344716c0e8af89
Cecile Muller <cecilemuller@users.noreply.github.com>
2020-02-18 16:19:49 +0100

    chore: rebuild the package

--- packages/snapshot-dom/removeEmptyAttributes/browser.js
+++ packages/snapshot-dom/removeEmptyAttributes/browser.js
@@ -1,1 +1,1 @@
-"use strict";window.snapshotRemoveEmptyAttributes=function t(s){if(Array.isArray(s.childNodes)&&(s.childNodes=s.childNodes.map(t)),s&&s.attributes){const t=[];for(const e in s.attributes)s.attributes[e]||t.push(e);t.forEach(t=>delete s.attributes[t])}return s};
+"use strict";window.snapshotRemoveEmptyAttributes=function t(e){if("object"==typeof e&&null!==e&&(Array.isArray(e.childNodes)&&(e.childNodes=e.childNodes.map(t)),e&&e.attributes)){const t=[];for(const s in e.attributes)e.attributes[s]||t.push(s);t.forEach(t=>delete e.attributes[t])}return e};
