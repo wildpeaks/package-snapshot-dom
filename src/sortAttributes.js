d71df9ee7b53ef1cec69c6339ea2a661a32d6016
Cecile Muller <cecilemuller@users.noreply.github.com>
2020-03-24 17:41:47 +0100

    chore: re-format using Prettier 2

--- src/sortAttributes.js
+++ src/sortAttributes.js
@@ -2,13 +2,13 @@
 "use strict";
 
 function sortAttributeValue(value) {
-	return (typeof value === "string") ? value.trim().replace(/[\s]+/g, " ").split(" ").sort().join(" ") : value;
+	return typeof value === "string" ? value.trim().replace(/[\s]+/g, " ").split(" ").sort().join(" ") : value;
 }
 
 function sortAttributes(tree, attributeIds) {
 	if (typeof tree === "object" && tree !== null && Array.isArray(attributeIds)) {
 		if (Array.isArray(tree.childNodes)) {
-			tree.childNodes.forEach(child => sortAttributes(child, attributeIds));
+			tree.childNodes.forEach((child) => sortAttributes(child, attributeIds));
 		}
 		if (tree && tree.attributes) {
 			for (const key in tree.attributes) {
